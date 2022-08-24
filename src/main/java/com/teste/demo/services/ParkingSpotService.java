eb6a62bd2240e190a33b5ee0273873f5e2032afa
Maria Mariana Cagnoni <mariamariana.cagnoni@gmail.com>
2022-08-24 15:32:40 -0300

    verifying the method POST

--- src/main/java/com/teste/demo/services/ParkingSpotService.java
+++ src/main/java/com/teste/demo/services/ParkingSpotService.java
@@ -1,8 +1,11 @@
 package com.teste.demo.services;
 
 
+import com.teste.demo.models.ParkingSpotModel;
 import com.teste.demo.repositories.ParkingSpotRepository;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
  public class ParkingSpotService {
@@ -14,4 +17,28 @@ import org.springframework.stereotype.Service;
     public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
         this.parkingSpotRepository = parkingSpotRepository;
     }
+
+
+    @Transactional // garante o rollback para evitar dados quebrados
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel){
+
+        return  parkingSpotRepository.save(parkingSpotModel);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+
+    }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+    return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+
+    }
+
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment,block);
+
+
+    }
 }
+
